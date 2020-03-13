986c91fe84c5ff38de761c5d9305cd50575dd8e3
ODanyor <dubashit@bk.ru>
2020-03-13 03:17:48 -0400

    Project visit link

--- src/components/Project/Illustration.js
+++ src/components/Project/Illustration.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./Illustration.css";
+import foreign from "../../static/images/foreign.png";
+
+const VisitProject = ({ link }) => {
+  return (
+    <a rel="noopener noreferrer" target="_blank" href={link} title={link}>
+      <div className="illustration-link">
+        <div className="illustration-link-sub">
+          <img alt="link" src={foreign} width="27px" height="auto" />
+          <p>Visit</p>
+        </div>
+      </div>
+    </a>
+  );
+};
 
 const Illustration = ({ img, link }) => {
   return (
     <div className="illustration-container">
-      <a rel="noopener noreferrer" target="_blank" href={link} title={link}>
-        <img alt="media" src={img} width="100%" height="auto" />
-      </a>
+      <img alt="media" src={img} width="100%" height="auto" />
+      {link ? <VisitProject link={link} /> : null}
     </div>
   );
 };

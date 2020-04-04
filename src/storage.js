f40a150c464b771c287d97c2682f053abe24f30b
ODanyor <dubashit@bk.ru>
2020-04-04 06:23:15 -0400

    Rest updates

--- src/storage.js
+++ src/storage.js
@@ -9,7 +9,7 @@ export const storage = [
     img: moneytracker,
     liveLink: "https://moneytrackerapp-0000.web.app/",
     description: {
-      githubLink: "https://github.com/ODanyor/MoneyTrackerApp",
+      githubLink: "https://github.com/ODanyor/money-tracker-app",
       title: "Money Tracker App",
       content:
         "This application is tracking for your expenses and profits. According to the added transactions it will build the diagrams for the better data analyzing. To test this app, first you need to authenticate yourself. The application was built by React, Redux, Firebase. For detailed information, please, check github repository. (Warning! The app is not responsive yet)"
@@ -19,7 +19,7 @@ export const storage = [
     img: lebonnet,
     liveLink: "https://lebonnet-0000.firebaseapp.com",
     description: {
-      githubLink: "https://github.com/ODanyor/LeBonnet",
+      githubLink: "https://github.com/ODanyor/lebonnet-clone",
       title: "Le Bonnet Clone By Dany",
       content:
         "This is a modern ecommerce concept app made with React.js. Moreoften I work on the most important features to get different experiences. This is very nice built Ecommerce Website with such cool features as cart, authorization and authentication, post new products, etc. I've spent about 1-2weeks to finish it."
@@ -28,7 +28,7 @@ export const storage = [
   {
     img: socialapp,
     description: {
-      githubLink: "https://github.com/ODanyor/SocialApp",
+      githubLink: "https://github.com/ODanyor/social-app",
       title: "Full Stack Social App",
       content:
         "Very cool social app for posting tweats and liking them. App provided by automatic notification system on likes and posts. Was built by React, Redux, Firebase cloud forestore, firebase functions and more small libries."
@@ -36,9 +36,9 @@ export const storage = [
   },
   {
     img: gameguides,
-    liveLink: "https://odanyor.github.io/GameGuidez",
+    liveLink: "https://odanyor.github.io/game-guidez",
     description: {
-      githubLink: "https://github.com/ODanyor/GameGuidez",
+      githubLink: "https://github.com/ODanyor/game-guidez",
       title: "GameGuides",
       content:
         "Basicly built for using Authorization and Authentication system of Firebase. Also authenticated users are able to post posts and all posts will be displayed in live time for all online users."
@@ -46,9 +46,9 @@ export const storage = [
   },
   {
     img: hackernews,
-    liveLink: "https://odanyor.github.io/HackerNews",
+    liveLink: "https://odanyor.github.io/hacker-news",
     description: {
-      githubLink: "https://github.com/ODanyor/HackerNews",
+      githubLink: "https://github.com/ODanyor/hacker-news",
       title: "HackerNews api using website",
       content:
         "This application shows different news basicly about IT sphere. It's enough small project but uses many react functionalities, such as React hooks, context and lazyLoading just like this portfolio."
